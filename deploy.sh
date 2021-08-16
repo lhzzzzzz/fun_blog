c263d44dc7f3bf4a511a961b5ba384c8d8f4a9ba
lhzzzzzz <542169991@qq.com>
2021-08-16 23:19:50 +0800

    modify

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io  USERNAME=你的用户名 
-git@github.com:lhzzzzzz/lhzzzzzz.github.io.git master
+git push -f git@github.com:lhzzzzzz/lhzzzzzz.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
