d39bcf0ac09e84b82477d5e327e92caf2e231a4e
liuhanzhe <542169991@qq.com>
2022-04-22 10:33:29 +0800

    modify

--- deploy.sh
+++ deploy.sh
@@ -25,11 +25,13 @@ echo 'pill0w.xyz' > CNAME
 git init 
 
 cp ../baidu_verify_code-8uVaS4w7Pu.html ./
-git add -A
-git commit -m 'deploy'
+scp -r D:\fun_blog\dist\* root@124.220.17.207:/www/wwwroot/pill0w.xyz
+KLlhz199048!
+#git add -A
+#git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io  USERNAME=你的用户名 
-git push -f git@github.com:lhzzzzzz/lhzzzzzz.github.io.git master
+#git push -f git@github.com:lhzzzzzz/lhzzzzzz.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
